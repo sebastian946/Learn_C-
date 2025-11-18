30b3448af5ef889873c4052b81d076718fa9de4f
sebastian946 <jhoan1113@hotmail.com>
2025-11-17 23:31:14 -0500

    Learn aboout types for c++

---
+++ types/types.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(){
+    unsigned short a = 0b1010101010;
+    printf("%hu\n", a);
+    int b = 0123;
+    printf("%d\n", b);
+    unsigned long long d = 0xFFFFFFFFFFFFF;
+    printf("%llu\n", d);
+}
